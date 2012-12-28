11a69aa9145095e362803c9a019c383df8804d98
Bob Weigel <rweigel@gmu.edu>
2012-12-28 07:47:27 -0500

    Clean up

--- tsdsgen-default.js
+++ tsdsgen-default.js
@@ -2,9 +2,10 @@ var Start   = '2012-10-01';
 var Stop    = '2012-10-10';
 var Cadence = '';
 	
-var CatalogDescription = ["USGS Real Time Magnetometer Measurements", "", "USGS/MAG/1M",""];
+var CatalogDescription = {Name:"USGS Real Time Magnetometer Measurements", ID:"USGS/MAG/1M",Description:""};
 var DatasetDescription = [
-						  ["BDT one-minute","","BDT1m","bdtvmin","BDT","bdt","Boulder Test"],                           ["BOU one-minute","","BOU1m","bouvmin","BOU","bou","Boulder"],
+						  ["BDT one-minute","","BDT1m","bdtvmin","BDT","bdt","Boulder Test"],                           
+						  ["BOU one-minute","","BOU1m","bouvmin","BOU","bou","Boulder"],
 						  ["BRT test one-minute","","BRT1m","brtvmin","BRT","brt","Barrow Test"],
 						  ["BRW one-minute","","BRW1m","brwvmin","BRW","brw","Barrow"],
 						  ["BSL one-minute","","BSL1m","bslvmin","BSL","bsl","Stennis Space Center"],
@@ -15,9 +16,10 @@ var DatasetDescription = [
 
 var Template           = "http://magweb.cr.usgs.gov/data/magnetometer/$5/OneMinute/$6%Y%m%dvmin.min";
 var ColumnLabels       = ["DATE","TIME","DOY","$5H","$5D","$5Z","$5F"];
-var ColumnLongname     = ["$7 H","$7 D","$7 Z","$7 F"];
-var ColumnGroupings    = ["$5_HDZ","$5_HDZ","$5_HDZ","$5_F"];
+var ColumnLongnames    = ["Date","Time","$7 H","$7 D","$7 Z","$7 F"];
+var ColumnUnits        = ["Gregorian","UTC","nT","nT","nT","nT"];
+var ColumnGroupings    = ["Time","Time","$5_HDZ","$5_HDZ","$5_HDZ","$5_F"];
 var LineTemplate       = "%Y-%m=%d %h:%M:%s.%ss %j %.2f %.2f %.2f %.2f";
 var LineRegex          = "^[0-9][0-9][0-9][0-9]";
-var ColumnGroupnames   = {"$5_XYZ":"Flux-gate Magnetometer Data","$5_F":"Proton Magnetometer Data"};
-var ColumnGrouptypes   = {"$5_XYZ":"Vector","$5_F":"Scalar"};
+var ColumnGroupnames   = {"Time":"Date and Time","$5_XYZ":"Flux-gate Magnetometer Data","$5_F":"Proton Magnetometer Data"};
+var ColumnGrouptypes   = {"Time":"Time","$5_XYZ":"Vector","$5_F":"Scalar"};
