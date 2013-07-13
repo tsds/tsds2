f15d5f07921c6ac96ef2710717273f0f8b4101b6
Bob Weigel <rweigel@gmu.edu>
2013-07-13 16:03:46 -0400

    New files

---
+++ presets/NSSDC_DE2_MAG.js
@@ -0,0 +1,56 @@
+id="NSSDC/DE2/MAG";
+Presets[id] = {};
+
+// For Autoplot, for now.
+Presets[id].PlotColumns = 7;
+
+Presets[id].CatalogName = "Magnetic and Electric Field data from Dynamics Explorer 2";
+Presets[id].CatalogID   = id;
+
+Presets[id].Datasets = [["DE2/MAG"]];
+
+						 
+Presets[id].URLTemplate = "ftp://spdf.gsfc.nasa.gov/pub/data/de/de2/magnetic_electric_fields_vefi_magb/dc_500ms_ascii/data/$Y$j_de2_vefi_magb_dc_500ms.asc";
+
+Presets[id].CatalogDescription = "0.5-sec. averaged magnetic and electric field vectors from NASA's DE-2 spacecraft (309-1012 km altitude, 89.9 deg inclination, 98 min orbital period) taken between August 15, 1981 and February 16, 1983."; // Could be a URL or [{title:"",description:""}] or {title:"",description:""}
+Presets[id].CatalogDescriptionURL = "http://ftpbrowser.gsfc.nasa.gov/de2_mag.html";
+
+Presets[id].StartDates       = ["1981-08-15"];
+Presets[id].StopDates        = ["1983-02-16"];
+
+Presets[id].DatasetName        = "$2";
+Presets[id].DatasetID          = "$1";
+Presets[id].DatasetDescription = ""; // Could be a URL or [{title:"",description:""}] or {title:"",description:""}
+
+Presets[id].TimeColumns      = "1";
+Presets[id].TimeFormat       = "$S";
+Presets[id].TimeUnits        = "Second";
+Presets[id].TimeLabels       = "seconds since 1981-08-15";
+
+Presets[id].ColumnLabels       = 
+Presets[id].ColumnLongnames    = Presets[id].ColumnLabels;
+Presets[id].ColumnUnits        = ["mV/m,mV/m,nT,nT,nT,nT,nT,nT,km,degrees,degrees,hour,degrees"];
+Presets[id].ColumnGroupings    = [];
+
+Presets[id].DataColumns      = "1,2,3,4,5,6,7,8,9,10,11,12,13"
+Presets[id].DataLabels       = "EX (SPC),EY (SPC),Bx (SPC),By (SPC),Bz (SPC),Bx-model (SPC),By-model (SPC),Bz-model (SPC),Altitude,Latitude,Longitude,MLT,INVariant LAT.";
+Presets[id].DataNames        = Presets[id].DataLabels;
+Presets[id].DataIDs          = Presets[id].DataLabels.replace("(|)","").replace(" ","");
+
+Presets[id].DataValues       = "";
+Presets[id].DataTypes        = "f,f,f,f,f,f,f,f,f,f,f,f,f";
+Presets[id].DataUnits        = "mV/m,mV/m,nT,nT,nT,nT,nT,nT,km,degrees,degrees,hour,degrees";
+Presets[id].DataRenderings   = "%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f";
+Presets[id].DataFillValues   = "99999.99, 999999.9, 999999.9 , 999999.9, 999999.9, 999999.9, 999999.9, 9999.999, 9999.999, 9999.999, 999.99, 9999.999, 9999.999"
+
+Presets[id].DataGroupIDs     = ""
+Presets[id].DataGroupNames   = ""
+Presets[id].DataGroupLabels  = ""
+
+Presets[id].SkipLines        = "0";
+Presets[id].LineRegEx        = "^[0-9]";
+Presets[id].CommentCharacter = "";
+Presets[id].DataDelimiter    = "\\s";
+Presets[id].DataLineFormat   = "";
+
+Presets[id].IOSP             = "lasp.tss.iosp.ColumnarAsciiReader";
---
+++ presets/USGS_RT_PT1S.js
@@ -0,0 +1,67 @@
+id = "USGS/RT/PT1S";
+Presets[id] = {};
+
+Presets[id].PlotColumns = 4;
+
+Presets[id].URLTemplate        = "http://magweb.cr.usgs.gov/data/magnetometer/$1/OneSecond/$5%Y%m%dvsec.sec";
+
+Presets[id].CatalogName        = "USGS Real Time 1-Second Magnetometer Measurements";
+Presets[id].CatalogID          = id;
+
+Presets[id].CatalogDescription = "Catalog derived by inspection of http://magweb.cr.usgs.gov/data/magnetometer/."
+Presets[id].CatalogDescriptionURL = "http://magweb.cr.usgs.gov/data/magnetometer/";
+
+Presets[id].StartDates       = ["2013-05-01"];
+Presets[id].StopDates        = ["2013-07-09"];
+
+Presets[id].Datasets         = [
+						  ["BDT","BDT one-minute","BDT1m","bdtvmin","bdt","Boulder Test"],                           
+						  ["BOU","BOU one-minute","BOU1m","bouvmin","bou","Boulder"],
+						  ["BRT","BRT one-minute","BRT1m","brtvmin","brt","Barrow Test"],
+						  ["BRW","BRW one-minute","BRW1m","brwvmin","brw","Barrow"],
+						  ["BSL","BSL one-minute","BSL1m","bslvmin","bsl","Stennis Space Center"],
+						  ["CMO","CMO one-minute","CMO1m","cmovmin","cmo","College"],
+						  ["DED","DED one-minute","DED1m","dedvmin","ded","Deadhorse"],
+						  ["FRD","FRD one-minute","FRD1m","frdvmin","frd","Fredericksburg"],
+						  ["FRN","FRN one-minute","FRN1m","frnvmin","frn","Fresno"],
+						  ["GUA","GUA one-minute","GUA1m","guavmin","gua","Guam"],
+						  ["HON","HON one-minute","HON1m","honvmin","hon","Honolulu"],
+						  ["KGI","KGI one-minute","KGI1m","kgivmin","kgi","King Sejong Island"],
+						  ["NEW","NEW one-minute","NEW1m","newvmin","new","Newport"],
+						  ["SHU","SHU one-minute","SHU1m","shuvmin","shu","Shumagin"],
+						  ["SIT","SIT one-minute","SIT1m","sitvmin","sit","Sitka"],
+						  ["SJG","SJG one-minute","SJG1m","sjgvmin","sjg","San Juan"],
+						  ["TUC","TUC one-minute","TUC1m","tucvmin","tuc","Tucson"],
+						 ];
+
+Presets[id].DatasetName        = "$2";
+Presets[id].DatasetID          = "$1";
+Presets[id].DatasetDescription = "Real-time ground magnetometer measurements from USGS."; 
+Presets[id].DatasetDescriptionURL = "http://geomag.usgs.gov/products/";
+
+Presets[id].TimeColumns      = "1,2";
+Presets[id].TimeFormat       = "$Y-$m-$d,$H:$M:$S.$(millis)";
+Presets[id].TimeUnits        = "Gregorian,UT";
+Presets[id].TimeLabels       = "Date,Time";
+
+Presets[id].DataColumns      = "3,(4,5,6),7";
+Presets[id].DataIDs          = "DOY,(H,D,Z),F";
+Presets[id].DataNames        = "Day of Year,($6 H,$6 D,$6 Z),$6 F";
+Presets[id].DataLabels       = "DOY,($1H,$1D,$1Z),$1F";
+Presets[id].DataValues       = "'',(0.1,0.2,0.3),''";
+Presets[id].DataTypes        = "d,f,f,f,f";
+Presets[id].DataUnits        = "Gregorian,nT,nT,nT,nT";
+Presets[id].DataRenderings   = "%j,%.2f,%.2f,%.2f,%.2f";
+Presets[id].DataFillValues   = ",99999.00,99999.00,99999.00,99999.00";
+
+Presets[id].DataGroupIDs     = "$1_HDZ"
+Presets[id].DataGroupNames   = "Magnetic Field Vector"
+Presets[id].DataGroupLabels  = ""
+
+Presets[id].SkipLines        = "25";
+Presets[id].LineRegex        = "^[0-9]";
+Presets[id].CommentCharacter = "^#";
+Presets[id].DataDelimiter    = "\\s";
+Presets[id].DataLineFormat   = "";
+
+Presets[id].IOSP             = "lasp.tss.iosp.ColumnarAsciiReader";
