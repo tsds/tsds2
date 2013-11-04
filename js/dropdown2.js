03ce510a22b35dcb3a63f8b5936b90d02f6a116b
Bob Weigel <rweigel@gmu.edu>
2013-11-04 15:43:16 -0500

    many changes to support tests and intermagnet

--- js/dropdown2.js
+++ js/dropdown2.js
@@ -10,7 +10,7 @@ function dropdown2(ids, names, funs, after, i, selected, callback) {
 		$("#"+ids[i]+"list").
 			click(function (){$("#"+ids[i]).attr('value','').css('color','black').autocomplete('search').focus();});
 	}
-	
+		
 	function ac(i) {
 		console.log(ids[i])
 		$("#"+ids[i]).autocomplete({
@@ -20,20 +20,33 @@ function dropdown2(ids, names, funs, after, i, selected, callback) {
 				if ($(this).attr('value') == "")
 					$("#"+ids[i]).attr('value','-'+names[i]+'-').css('color','black');
 			},
-			blur: function(event) {event.preventDefault();},
+			blur: function(event) {
+				//event.preventDefault();
+				},
+			//search: function (event) {funs[i].onselect();},
 			change: function(event,ui) {
 				console.log("dropdown2: Change event triggered on " + i);				
-				
+				$('input[id=' + ids[i] + ']').unbind('blur')
+				$('input[id=' + ids[i] + ']').blur(function () {
+					console.log("blur event");
+					var val = $('input[id=' + ids[i] + ']').parent().parent().attr('value');
+					var vallast = $('input[id=' + ids[i] + ']').parent().parent().attr('valuelast');
+
+					console.log("Value: " + val)
+					console.log("Value Last: " + vallast)
+					if (typeof(val) === "undefined") return;
+					if (val !== vallast)
+						$('input[id=' + ids[i] + ']').val(val).data("autocomplete")._trigger("select",event,{item:val});
+				});
 
 				if (ui.item == null) {
 					ui.item = {};
-					ui.item.value = $(this).attr("value");
+					ui.item.value = $('input[id=' + ids[i] + ']').parent().parent().attr('value');
 					return;
 				}
 
 				$(after+i).attr('name',ids[i]).attr('value',ui.item.value);
-				console.log(ui.item.value)
-				console.log(ui)
+				
 				// This is to deal with the fact that change() is called on a blur event.
 				if (ui.item.value !== ui.item.valuelast) {
 					console.log("           and value has changed.  Removing all following drop-downs.");
@@ -43,6 +56,7 @@ function dropdown2(ids, names, funs, after, i, selected, callback) {
 				} else {
 					console.log("           and value has not changed.  Not removing " + after+(i+1));
 				}
+
 			},			
 			select: function(event,ui) {
 						console.log("dropdown2: Select event triggered on " + i + " with value " + ui.item.value);
@@ -51,8 +65,21 @@ function dropdown2(ids, names, funs, after, i, selected, callback) {
 						// $("#catalog").val("cdaweb").data("autocomplete")._trigger("select",event,{item:"cdaweb"})
 						// In this case, use event.targe.value as ui.item is undefined.
 						
+						
+						console.log(ui.item.value)
+						console.log(ui.item.valuelast)
 						val = ui.item.value || event.target.value;
+						//var val = $('input[id=' + ids[i] + ']').parent().parent().attr('value');
+						var vallast = $('input[id=' + ids[i] + ']').parent().parent().attr('valuelast');
+
 						console.log("Val: " + val)
+						console.log("Val last: " + vallast)
+						if (val === ui.item.valuelast) {return;}
+
+						$('input[id=' + ids[i] + ']').parent().parent().attr('value',val)
+
+						//if (typeof(val) === "undefined") return;
+						
 						$(after+i).attr('name',ids[i]).attr('value',val);
 						console.log(location.hash === "")
 						if (location.hash === "") {
@@ -61,15 +88,12 @@ function dropdown2(ids, names, funs, after, i, selected, callback) {
 							var qs = $.parseQueryString();
 						}
 							
-							
-							console.log(qs)
-							//if ($("#dropdowns"+i).val().length > 0) {
-							qs[$("#dropdowns" + (i)).attr('name')] = $("#dropdowns"+i).val();
-							//}
-							for (j = i+1; j < ids.length; j++) {
-								console.log("Deleting "+$("#dropdowns" + (j)).attr('name'))
-								delete qs[$("#dropdowns" + (j)).attr('name')];
-							}
+						console.log(qs)
+						qs[$("#dropdowns" + (i)).attr('name')] = $("#dropdowns"+i).val();
+						for (j = i+1; j < ids.length; j++) {
+							console.log("Deleting "+$("#dropdowns" + (j)).attr('name'))
+							delete qs[$("#dropdowns" + (j)).attr('name')];
+						}
 						for (j = i+1; j < ids.length; j++) {
 							$("#dropdowns" + (j)).html('');
 							console.log("Deleting "+$("#dropdowns" + (j)).attr('name'))
@@ -78,17 +102,21 @@ function dropdown2(ids, names, funs, after, i, selected, callback) {
 						console.log(qs)
 						console.log("xSetting hashchange.byurledit to false");
 						$(window).hashchange.byurledit = false;
-						location.hash = decodeURIComponent($.param(qs));
 
+						location.hash = decodeURIComponent($.param(qs));
 						funs[i].onselect();
 						dropdown2(ids, names, funs, after, i+1, val, callback);
 						$(after+(i+1)).show();
-						if (ui.item)
+						$('input[id=' + ids[i] + ']').parent().parent().attr('valuelast',val)
+
+						if (val) {
 							ui.item.valuelast = ui.item.value;
+						}
 			},
 			minLength: 0
 		}).click(function () {
-			$(this).attr('value','').css('color','black').autocomplete('search');
+			if (!$(this).attr('value'))
+				$(this).attr('value','').css('color','black').autocomplete('search');
 		});
 	}
 
@@ -108,10 +136,25 @@ function dropdown2(ids, names, funs, after, i, selected, callback) {
 		$(this).parent().parent().attr('value',$(this).attr('value'));
 	});
 	
-
 	ac(i);
 	settoggle(i);
-	if (callback)
-		callback(ids[i],i);
+	$('input[id=' + ids[i] + ']').keypress(function(e) {
+		  console.log( "Handler for .keypress() called." );
+		    console.log(e.keyCode)
+		    var val = $(this).parent().parent().attr('value');
+		    //console.log(val)
+		    if(e.keyCode == 13)
+		    {
+		    	$('input[id=' + ids[i] + ']').val(val).data("autocomplete")._trigger("change",event,{item:val});
+		    	$('input[id=' + ids[i] + ']').blur();
+		    }
+	});
+
+	// If only one item, select it.
+	var list = funs[i](i,selected);
+	if (list.length == 1) {
+		$('input[id=' + ids[i] + ']').val(list[0].value).data("autocomplete")._trigger("select",event,{item:list[0].value});
+	}
+	if (callback) callback(ids[i],i);
 
 }
--- tsdsfe.js
+++ tsdsfe.js
@@ -1,4 +1,5 @@
-var debug   = true;
+var debug        = true;
+var debugcatalog = false;
 
 var fs      = require('fs');
 var request = require("request");
@@ -10,17 +11,12 @@ var xml2js  = require('xml2js');
 var port    = process.argv[2] || 8004;
 var http    = require('http');
 var url     = require('url');
+var util    = require('util');
 
 http.globalAgent.maxSockets = 100;  // Most Apache servers have this set at 100.
 
 //var plugin = require('./plugin.js');
 var DC = "http://localhost:7999/sync/";
-
-line       = "2001 01 02 0233 1 442 3 4 5 6";
-timeformat = "yyyy MM dd HHmm s SSS";
-timecols   = "1,2,3,4,5,6";
-timeformat = "yyyy MM dd";
-timecols   = "1,2,3";
 
 //console.log(plugin.extractLine(line,timeformat,timecols))
 app.use("/tsdsfe2/js", express.static(__dirname + "/js"));
@@ -48,7 +44,6 @@ app.get('/tsdsfe2/tsdsfe.jyds', function (req, res) {
 	}
 });
 
-
 app.get('/', function (req, res) {
 	if (Object.keys(req.query).length === 0) {
 		res.contentType("html");
@@ -56,7 +51,6 @@ app.get('/', function (req, res) {
 		return;
 	}
 	handleRequest(req,res);
-
 });
 
 app.get('/tsdsfe2', function (req, res) {
@@ -66,12 +60,141 @@ app.get('/tsdsfe2', function (req, res) {
 		return;
 	}
 	handleRequest(req,res);
-
 });
 
 server.listen(port);
 
-if (process.argv[3]) {request("http://localhost:"+port+"/?"+process.argv[3], function (error, response, body) {console.log(body);});}
+function handleRequest(req, res) {
+	var options = parseOptions(req);
+
+	if (debug) console.log("Handling " + req.originalUrl);
+
+	var Nc = 0;
+	var N  = options.catalog.split(";").length;
+
+	if (N > 1) {
+		var catalogs   = options.catalog.split(";");
+		var datasets   = options.dataset.split(";");
+		var parameters = options.parameters.split(";");
+		var starts     = options.start.split(";");
+		var stops      = options.stop.split(";");
+
+		if (debug) console.log("Concatenated parameter request. N = "+N);
+		
+		var Options = [];
+		var str = JSON.stringify(options);
+		for (var i=0;i<N;i++) {
+			Options[i] = {};
+			Options[i] = JSON.parse(str);
+			Options[i].catalog    = catalogs[i];
+			Options[i].dataset    = datasets[i];
+			Options[i].parameters = parameters[i];
+			Options[i].start      = starts[i];
+			Options[i].stop       = stops[i];
+		}
+		catalog(Options[0], stream);
+		return;
+	}
+		
+	catalog(options, stream);
+
+	function stream(status, data) {
+		console.log("Stream called.")
+		console.log(Options);
+		// If more than one resp, this won't work.
+
+		if (options.attach === "true") {
+			//res.setHeader('Content-disposition', 'attachment; filename=data')
+		}
+
+		if (status == 0) {
+			if (!data.match(/^http/)) {
+				if (debug) console.log("Sending "+data)				
+				res.write(data);
+	    		Nc = Nc + 1;
+	    		if (N > 1) res.write("\n");
+
+	    		if (Nc == N) {
+		    		res.end();
+		    	} else {
+					catalog(Options[Nc], stream);
+		    	}
+				return;
+			}
+
+			if (debug) console.log("Streaming from\n"+data)
+			http.get(url.parse(data), function(res0) {
+				util.pump(res0,res);
+				return;
+			    var data = [];
+			    console.log(res0.headers)
+			    res.writeHeader(200, {"Content-Type": "text/plain"}); 
+			    //res.setHeader('Content-Disposition','attachment; filename='+res0.headers['content-disposition']);
+			    res0
+			    .on('data', function(chunk) {
+			        res.write(chunk);
+			        data = data+chunk;
+			        //if(!flushed) res0.pause();
+			        console.log("Got and wrote chunk of size "+chunk.length);
+			        //console.log(data)
+			    })
+			    .on('end', function() {
+				    	console.log('got end.');
+				    	Nc = Nc + 1;
+				    	//if (N > 1) res.write("\n");
+			    		if (Nc == N) {
+			    			console.log("Sending res.end().");
+			    			//res.write("\n")
+			    			//console.log(data)
+				    		res.end();
+				    	} else {
+				    		console.log("Calling catalog with Nc="+Nc);
+							//catalog(Options[Nc], stream);
+				    	}
+			    })
+			}).on('error', function () {
+		    	res.send(502);
+		    });
+		} else if (status == 301) {
+			res.redirect(301,data);
+		} else {
+			res.write(JSON.stringify(data));
+    		Nc = Nc + 1;
+    		if (N > 1) res.write("\n");
+    		if (Nc == N) {
+	    		res.end();
+	    	} else {
+				catalog(Options[Nc], stream);
+	    	}
+		}
+		if (debug) console.log("Sent response.");
+	}
+
+}
+
+
+function parseOptions(req) {
+	var options = {};
+    
+	function s2b(str) {if (str === "true") {return true} else {return false}}
+	function s2i(str) {return parseInt(str)}
+
+	options.all          = req.query.all          || req.query.body        || "/uploads/all.thredds";
+	options.catalog      = req.query.catalog      || req.body.catalog      || "^.*";
+	options.dataset      = req.query.dataset      || req.body.dataset      || "";
+	options.parameters   = req.query.parameters   || req.body.parameters   || "";
+	options.start        = req.query.start        || req.body.start        || "";
+	options.stop         = req.query.stop         || req.body.stop         || "";
+	options.return       = req.query.return       || req.body.return       || "stream";
+	options.attach       = req.query.attach       || req.body.attach       || "";
+	options.outformat    = req.query.outformat    || req.body.outformat    || "0";
+	options.filter       = req.query.filter       || req.body.filter       || "";
+	options.filterWindow = req.query.filterWindow || req.body.filterWindow || "0";
+	options.usecache     = s2b(req.query.usecache || req.body.usecache     || "true");
+	
+	return options;
+
+}
 
 function catalog(options, cb) {
 
@@ -82,9 +205,9 @@ function catalog(options, cb) {
 	var resp = [];
 
 	var data = fs.readFileSync(fname);
-	
+
 	parser.parseString(data, function (err, result) {
-		console.log(result)
+		//console.log(result)
 		//var catalogRefs = JSON.stringify(result["catalog"]["catalogRef"]);
 			var catalogRefs = result["catalog"]["catalogRef"];
 			if (debug) console.log("Found " + catalogRefs.length + " catalogRef nodes.");
@@ -113,12 +236,13 @@ function catalog(options, cb) {
 			resp = resp.filter(function(n){return n})
 
 			if (options.dataset === "") {
+				
 				if (resp.length == 1 && options.catalog.substring(0,1) !== "^") {
-					console.log("Fetching " + resp[0].href);
+					if (debug) console.log("Fetching " + resp[0].href);
 					request(resp[0].href, function (error, response, body) {
 						if (!error && response.statusCode == 200) {
 							parser.parseString(body, function (err, result) {
-								console.log(result["catalog"]["documentation"]);
+								if (debugcatalog) console.log(result["catalog"]["documentation"]);
 								for (var k = 0; k < result["catalog"]["documentation"].length;k++) {
 									resp[k].title = result["catalog"]["documentation"][k]["$"]["xlink:title"];
 									resp[k].link  = result["catalog"]["documentation"][k]["$"]["xlink:href"];
@@ -132,8 +256,10 @@ function catalog(options, cb) {
 				} else {
 					cb(200,resp);
 				}
+
 			} else {
 				dataset(options,resp,cb);
+
 			}
 		});
 
@@ -144,12 +270,13 @@ function dataset(options,resp,cb) {
 	var parser = new xml2js.Parser();
 	var j = 0;
 	var N = resp.length;
+	if (N == 0) {cb(200,"[]");return;}
+
 	var datasets = [];
 	var parents = [];
-
 	var dresp = [];
 	for (var i = 0; i < N;i++) {
-		console.log("Fetching " + resp[i].href);
+		if (debug) console.log("Fetching " + resp[i].href);
 		request(resp[i].href, function (error, response, body) {
 			if (!error && response.statusCode == 200) {
 				parser.parseString(body, function (err, result) {
@@ -185,11 +312,9 @@ function dataset(options,resp,cb) {
 								}
 							}
 						}
-
-						console.log(datasets)
+						if (debugcatalog) console.log(datasets)
 						if (options.parameters === "") {
 							dresp = dresp.filter(function(n){return n;});
-							console.log(datasets)
 							if (dresp.length == 1 && options.dataset.substring(0,1) !== "^") {
 								for (var k = 0; k < datasets[z]["documentation"].length;k++) {
 									dresp[k].title = datasets[z]["documentation"][k]["$"]["xlink:title"];
@@ -217,7 +342,7 @@ function parameter(options,datasets,catalogs,cb) {
 	var parents = [];
 	var cats = [];
 	var resp = [];
-
+	
 	for (var i = 0;i < datasets.length;i++) {
 		parameters = parameters.concat(datasets[i].variables[0].variable);
 		var parent = datasets[i]["$"];
@@ -235,7 +360,7 @@ function parameter(options,datasets,catalogs,cb) {
 		}
 	}							
 
-		for (var i = 0;i < parameters.length;i++) {
+	for (var i = 0;i < parameters.length;i++) {
 			
 			resp[i]           = {};
 			resp[i].value     = parameters[i]["$"]["id"];
@@ -244,13 +369,15 @@ function parameter(options,datasets,catalogs,cb) {
 			resp[i].dataset   = parents[i]["ID"];
 			resp[i].parameter = resp[i].value;
 			resp[i].dd        = parameters[i]["$"];
-						
-			if (!('urltemplate' in resp[i].dd)) {resp[i].dd.urltemplate = parents[i]["urltemplate"]}
-			if (!('timeformat' in resp[i].dd)) {resp[i].dd.timeformat = parents[i]["timeformat"]}
-			if (!('timecolumns' in resp[i].dd)) {resp[i].dd.timecolumns = parents[i]["timecolumns"]}
-			if (!('columns' in resp[i].dd)) {resp[i].dd.columns = parents[i]["columns"]}
-			if (!('start' in resp[i].dd)) {resp[i].dd.start = parents[i]["start"]}
-			if (!('stop' in resp[i].dd)) {resp[i].dd.stop = parents[i]["stop"]}
+
+			if (!('urltemplate' in resp[i].dd))  {resp[i].dd.urltemplate = parents[i]["urltemplate"]}
+			if (!('urlsouce' in resp[i].dd))     {resp[i].dd.urlsource = parents[i]["urlsource"]}
+			if (!('urlprocessor' in resp[i].dd)) {resp[i].dd.urlprocessor = parents[i]["urlprocessor"]}
+			if (!('timeformat' in resp[i].dd))   {resp[i].dd.timeformat = parents[i]["timeformat"]}
+			if (!('timecolumns' in resp[i].dd))  {resp[i].dd.timecolumns = parents[i]["timecolumns"]}
+			if (!('columns' in resp[i].dd))      {resp[i].dd.columns = parents[i]["columns"]}
+			if (!('start' in resp[i].dd))        {resp[i].dd.start = parents[i]["start"]}
+			if (!('stop' in resp[i].dd))         {resp[i].dd.stop = parents[i]["stop"]}
 
 			if (options.parameters !== "^.*") {				
 				if (options.parameters.substring(0,1) === "^") {
@@ -263,30 +390,55 @@ function parameter(options,datasets,catalogs,cb) {
 					}
 				}
 			}
-		}
+	}
+	
 		resp = resp.filter(function(n){return n});
+		if (typeof(resp[0]) === "undefined") {cb(200,"[]");return;}
 
 		var columns = resp[0].dd.timecolumns + "," + resp[0].dd.columns;
-		
-		// TODO: Do this for each part of resp.  Concatenate output.
+
 		if (options.start || options.stop) {
 			start = options.start || resp[0].dd.start;
 			stop  = options.stop  || resp[0].dd.stop;
-			if (Date.parse(options.start) < Date.parse(resp[0].dd.start)) {start = resp[0].dd.start}
-			if (Date.parse(options.stop) > Date.parse(resp[0].dd.stop)) {stop = resp[0].dd.stop}
+			console.log("Requested start :" + Date.parse(options.start));
+			console.log("DD start        :" + Date.parse(resp[0].dd.start));
+			console.log("Requested stop  :" + Date.parse(options.stop));
+			console.log("DD stop         :" + Date.parse(resp[0].dd.stop));
+
+			
+			if (Date.parse(stop) < Date.parse(start)) {
+				cb(500,"Stop time is before than start time");
+				return;
+			}
+			if (Date.parse(start) > Date.parse(stop)) {
+				cb(500,"Start time is later than stop time");
+				return;
+			}
+			
 			//console.log("template="+resp[0].dd.urltemplate+"&timeRange="+start+"/"+stop);
 
-			var urltemplate = url.parse(resp[0].dd.urltemplate,true);
-			//console.log(urltemplate)
-			console.log(options)
-			if (urltemplate.query.plugin) {
-				args = "plugin="+urltemplate.query.plugin;
+			var urltemplate  = resp[0].dd.urltemplate;
+			var urlprocessor = resp[0].dd.urlprocessor;
+			var urlsource    = resp[0].dd.urlsource;
+							
+			//console.log("urlprocessor: " + resp[0].dd.urlprocessor)
+			var args = "";
+			if (urlprocessor) {
+				args = "&plugin="+urlprocessor;
 			}
-			if (urltemplate.query.source) {
-				args = args + "&template="+urltemplate.query.source;
+			if (urltemplate) {
+				args = args + "&template="+urltemplate;
+			} 
+			if (urlsource) {
+				args = args + "&source="+urlsource;
 			}
-			var dc = DC+"?"+args+"&timeRange="+start+"/"+stop;
 
+			if (args) {
+				var dc = DC+"?"+args+"&timeRange="+start+"/"+stop;
+			} else {
+				var dc = DC+"?timeRange="+start+"/"+stop;
+			}
+			
 			if (options.return === "urilist") {
 				cb(0,dc+"&return=urilist");
 			}
@@ -307,92 +459,28 @@ function parameter(options,datasets,catalogs,cb) {
 			}
 
 			if (options.return === "stream") {				 
-				dc = dc+"&forceUpdate=true&forceWrite=true&return=stream"
+				dc = dc
+					+"&return=stream"
 					+"&lineRegExp="+ resp[0].dd.lineregex
 					+"&timecolumns="+resp[0].dd.timecolumns
 					+"&timeformat="+resp[0].dd.timeformat
 					+"&streamFilterReadColumns="+columns
-					+"&lineFormatter=formattedTime&outformat="+options.outformat
+					+"&streamFilterTimeFormat="+options.outformat
 					+"&streamFilterComputeFunction="+options.filter
 					+"&streamFilterComputeWindow="+options.filterWindow
 					+"&streamOrder=true"
+					+"&streamGzip=true"
 					;
-				
+
+				if (!options.usecache) dc = dc+"&forceUpdate=true&forceWrite=true"
+
 				//dc = dc+"&return=stream&lineRegExp="+resp[0].dd.lineregex + "&timecolumns="+resp[0].dd.timecolumns+"&timeformat="+resp[0].dd.timeformat+"&streamFilterReadColumns="+columns+"&lineFormatter=formattedTime&outformat="+options.outformat;
-				console.log(dc)
+				//console.log(dc)
 				cb(0,dc)
 			}
-			// If more than one resp, this won't work.
-			if (options.attach === "true") {
-			}
 		} else {
-			console.log("here")
 			cb(200,resp);
 		}
 		
 }
 
-function handleRequest(req, res) {
-	var options = parseOptions(req);
-
-	console.log("Handling " + req.originalUrl);
-	if (options.catalog.split(",").length > 1) {
-		//console.log(options.catalog.split(",")[0]);
-		//xreq.query = options.query;
-		//xreq.query.catalog = options.catalog.split(",")[0];
-		//xreq.query.dataset = options.dataset.split(",")[0];
-		//xreq.query.parameters = options.parameters.split(",")[0];
-		//xreq.query.start = options.start.split(",")[0];
-		//xreq.query.stop = options.stop.split(",")[0];
-		// for (var i=0;i<options.catalog.split(",")[0];i++) handleRequest(xreq,res);
-		//return;
-	}
-		
-	catalog(options, function (status, data) {
-		if (status == 0) {
-			if (!data.match(/^http/)) {
-				console.log("Sending "+data)				
-				res.send(data);
-				return;
-			}
-			console.log("Streaming from "+data)
-			http.get(url.parse(data), function(res0) {				 
-			    var data = [];
-			    res0.on('data', function(chunk) {
-			        res.write(chunk);
-			    }).on('end', function() {
-			    	res.end();
-			    })
-			}).on('error', function () {
-		    	res.send(502);
-		    });
-		} else if (status == 301) {
-			res.redirect(301,data);
-		} else {
-			res.send(status,data);
-		}
-		if (debug) console.log("Sent response.");
-	});
-}
-
-function parseOptions(req) {
-	var options = {};
-    
-	function s2b(str) {if (str === "true") {return true} else {return false}}
-	function s2i(str) {return parseInt(str)}
-
-	options.all        = req.query.all        || req.query.body      || "/uploads/all.thredds";
-	options.catalog    = req.query.catalog    || req.body.catalog    || "^.*";
-	options.dataset    = req.query.dataset    || req.body.dataset    || "";
-	options.parameters = req.query.parameters || req.body.parameters || "";
-	options.start      = req.query.start      || req.body.start      || "";
-	options.stop       = req.query.stop       || req.body.stop       || "";
-	options.return     = req.query.return     || req.body.return     || "stream";
-	options.attach     = req.query.attach     || req.body.attach     || "";
-	options.outformat  = req.query.outformat  || req.body.outformat  || "0";
-	options.filter     = req.query.filter     || req.body.filter     || "";
-	options.filterWindow = req.query.filterWindow     || req.body.filterWindow     || "0";
-	
-	return options;
-
-}
