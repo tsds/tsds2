52240ea3ddcf797e0461b6e804b40363224604bd
Bob Weigel <rweigel@gmu.edu>
2013-11-25 13:56:14 -0500

    Escape single quote

--- catalogs/intermagnet/metadata.js
+++ catalogs/intermagnet/metadata.js
@@ -49,7 +49,7 @@ function unz(i,N,cb) {
 	    		var meta = lista[i].replace(/ /g,",") + "," + coordsys + "," + lat + "," + long + "," + source + "," + name;
 	    		var line = meta.replace(/\s{2,}|\||^M/g,"");
 	    		//console.log(line);
-	    		listf[i] = line.replace("'","\'");
+	    		listf[i] = line.replace(/'/g,"\\'");
 	    		unz.N = unz.N+1;
 	    		process.stderr.write(unz.N+ "/" + N+"\n");
 	    		if (unz.N === N) cb();
