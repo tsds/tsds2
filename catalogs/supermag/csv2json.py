dce0de0a8e52d024fcb722452fd0991787d65ea5
Robert Weigel <rweigel@gmu.edu>
2014-08-18 19:01:12 -0400

    Moving old code

--- catalogs/supermag/csv2json.py
+++
@@ -1,7 +0,0 @@
-import csv
-import json
-
-f = open( 'SuperMAG.txt', 'r' )
-reader = csv.DictReader( f, fieldnames = ( "id","lat","lng","start","stop","name" ) )
-out = json.dumps( [ row for row in reader ] )
-print out
