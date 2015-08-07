150835e4b4ed43f0c324888cfb6d98778bfbc7c0
Bob Weigel <rweigel@gmu.edu>
2015-08-06 21:45:43 -0400

    Adding large data tests

---
+++ tsdsfe/test/data-large-tests.js
@@ -0,0 +1,18 @@
+i = i+1;
+tests[i] = {};
+tests[i].url = "catalog=USGS/Mag/RT/PT1S&dataset=BDT&parameters=H&start=-P2D&stop=P0D";
+tests[i].test = function (data) {
+	var len = data.toString().split("\n").length;
+	console.log("File should have 172801 newlines.  Found: " + len );
+	if (len == 172801) {return true;} else {return false}
+};
+
+i = i+1;
+tests[i] = {};
+tests[i].url = "catalog=INTERMAGNET/PT1S&dataset=AAE&parameters=X&start=-P2D&stop=2012-11-30";
+tests[i].test = function (data) {
+	var len = data.toString().split("\n").length;
+	console.log("File should have 172801 newlines.  Found: " + len );
+	if (len == 172801) {return true;} else {return false}
+};
+tests[i].note = ''
