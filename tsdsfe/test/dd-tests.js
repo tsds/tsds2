24129af621581ed13ec105e996df1fb347b89e0a
Robert Weigel <rweigel@gmu.edu>
2015-12-23 09:52:17 -0500

    new test for dd

--- tsdsfe/test/dd-tests.js
+++ tsdsfe/test/dd-tests.js
@@ -16,3 +16,20 @@ tests[i].test = function (data) {
 		return false
 	}
 }
+
+i = i + 1
+tests[i] = {}
+tests[i].url = "dd=" 
+	+ encodeURIComponent("uri=__SERVER__test/data/2015-11-20.txt&start=2015-11-20&stop=2015-11-21&columns=2,3")
+	+ "&return=tsds&format=json"
+tests[i].test = function (data) {
+	dataj = JSON.parse(data)
+	var testv = dataj["catalog"]["timeCoverage"][0]["Start"][0]
+	var testv0 = "2015-11-20"
+	console.log("Response Start value should be " + testv0 + ".  Found " + testv + ".")	
+	if (testv === testv0) {
+		return true
+	} else {
+		return false
+	}
+}
