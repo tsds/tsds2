69987261449564eb2c845bb2d5e6dcf8c836eb0d
Bob Weigel <rweigel@gmu.edu>
2016-02-13 13:01:39 -0500

    Fix test

--- tsdsfe/test/dd-tests.js
+++ tsdsfe/test/dd-tests.js
@@ -7,8 +7,8 @@ tests[i].url = "dd="
 	+ "&return=tsds&format=json"
 tests[i].test = function (data) {
 	dataj = JSON.parse(data)
-	var testv0 = dataj["catalog"]["timeCoverage"][0]["Start"][0]
-	var testv1 = dataj["catalog"]["timeCoverage"][0]["End"][0]
+	var testv0 = dataj["catalog"]["dataset"][0]["timeCoverage"][0]["Start"][0]
+	var testv1 = dataj["catalog"]["dataset"][0]["timeCoverage"][0]["End"][0]
 	var test0 = "2015-11-20T00:00:01.985Z"
 	var test1 = "2015-11-20T23:59:03.880Z"
 	console.log("Response Start/End values should be " + testv0 + "/" + testv0 + ".  Found " + test0 + "/" + test1 + ".")	
