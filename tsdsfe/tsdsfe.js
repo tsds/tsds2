cc058524cd71f0023c7cba90f045adf049f034bb
Bob Weigel <rweigel@gmu.edu>
2016-05-25 14:03:07 -0400

    Adding delimiter option

--- tsdsfe/tsdsfe.js
+++ tsdsfe/tsdsfe.js
@@ -2503,6 +2503,7 @@ function parameter(datasets, res, cb) {
 				+"&streamFilterWriteComputeFunction=" + res.opts.filter
 				+"&streamFilterWriteComputeFunctionWindow=" + res.opts.filterWindow
 				+"&streamFilterWriteComputeFunctionExcludes=" + (resp[0].dd.fillvalue || "")
+				+"&streamFilterWriteDelimiter=,"
 				+"&streamOrder=true"
 				+"&streamGzip=false"
 				;
